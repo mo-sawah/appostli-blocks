20fc0733d91b42fdd762208ff8dc6eb6682ef4ff
Mohamed Sawah <mosawah@sawahsolutions.com>
2026-04-20 21:57:11 +0300

    ol.l;

--- appostli-blocks.php
+++ appostli-blocks.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://sawahsolutions.com
  * Author:      Mohamed Sawah
  * Author URI:  https://sawahsolutions.com
- * Version:     1.0.2
+ * Version:     1.0.3
  * Text Domain: appostli-blocks
  */
 
--- widgets/retro-divider.php
+++ widgets/retro-divider.php
@@ -35,7 +35,11 @@ class Appostli_Retro_Divider extends \Elementor\Widget_Base {
             [
                 'label' => esc_html__( 'Pixel Color', 'appostli-blocks' ),
                 'type' => \Elementor\Controls_Manager::COLOR,
-                'default' => '#FF00FF', // The pink from the mockup
+                'default' => '#FF00FF',
+                'selectors' => [
+                    // Save the chosen color as a CSS variable on the wrapper
+                    '{{WRAPPER}}' => '--retro-divider-color: {{VALUE}};',
+                ],
             ]
         );
 
@@ -48,16 +52,15 @@ class Appostli_Retro_Divider extends \Elementor\Widget_Base {
                 'range' => [
                     'px' => [ 'min' => 2, 'max' => 20, 'step' => 1 ],
                 ],
-                // Defaulting to 4px for that chunky retro vibe
                 'default' => [ 'unit' => 'px', 'size' => 4 ], 
                 'selectors' => [
-                    // This CSS mathematically draws the linked ++++ crosses perfectly
+                    // Draw the overlapping ++++ pattern using the CSS variable
                     '{{WRAPPER}} .retro-cross-line' => '
                         height: calc({{SIZE}}{{UNIT}} * 3);
                         background-image: 
-                            linear-gradient(90deg, transparent 50%, {{color.VALUE}} 50%),
-                            linear-gradient(90deg, {{color.VALUE}} 100%, transparent 100%),
-                            linear-gradient(90deg, transparent 50%, {{color.VALUE}} 50%);
+                            linear-gradient(90deg, transparent 50%, var(--retro-divider-color) 50%),
+                            linear-gradient(90deg, var(--retro-divider-color) 100%, transparent 100%),
+                            linear-gradient(90deg, transparent 50%, var(--retro-divider-color) 50%);
                         background-size: 
                             calc({{SIZE}}{{UNIT}} * 2) {{SIZE}}{{UNIT}},
                             calc({{SIZE}}{{UNIT}} * 2) {{SIZE}}{{UNIT}},
@@ -67,7 +70,7 @@ class Appostli_Retro_Divider extends \Elementor\Widget_Base {
                             0 {{SIZE}}{{UNIT}},
                             0 calc({{SIZE}}{{UNIT}} * 2);
                         background-repeat: repeat-x;
-                        image-rendering: pixelated; /* Forces sharp edges on high-res screens */
+                        image-rendering: pixelated;
                     ',
                 ],
             ]
@@ -77,7 +80,6 @@ class Appostli_Retro_Divider extends \Elementor\Widget_Base {
     }
 
     protected function render() {
-        // We only need one single div now! The CSS does all the heavy lifting.
         ?>
         <div class="appostli-retro-divider-wrapper" style="width: 100%;">
             <div class="retro-cross-line"></div>

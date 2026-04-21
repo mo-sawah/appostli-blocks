63ad7d7e4aa7616f12477def6c988adc9a7d16ef
Mohamed Sawah <mosawah@sawahsolutions.com>
2026-04-21 04:36:20 +0300

    hgfd

--- appostli-blocks.php
+++ appostli-blocks.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://sawahsolutions.com
  * Author:      Mohamed Sawah
  * Author URI:  https://sawahsolutions.com
- * Version:     1.0.11
+ * Version:     1.0.9
  * Text Domain: appostli-blocks
  */
 
@@ -43,48 +43,3 @@ add_action( 'wp', function() {
     $appostli_shown_posts = array();
 });
 
-/**
- * AJAX Handler for Appostli Load More
- */
-add_action('wp_ajax_appostli_load_more', 'appostli_load_more_scripts');
-add_action('wp_ajax_nopriv_appostli_load_more', 'appostli_load_more_scripts');
-
-function appostli_load_more_scripts() {
-    $paged = isset($_POST['page']) ? intval($_POST['page']) : 1;
-    $posts_per_page = isset($_POST['posts_per_page']) ? intval($_POST['posts_per_page']) : 4;
-    
-    $args = array(
-        'post_type'      => 'post',
-        'post_status'    => 'publish',
-        'posts_per_page' => $posts_per_page,
-        'paged'          => $paged,
-    );
-
-    // If we are on a category or author archive, pass those variables
-    if (!empty($_POST['cat'])) $args['cat'] = intval($_POST['cat']);
-    if (!empty($_POST['author'])) $args['author'] = intval($_POST['author']);
-
-    $query = new WP_Query($args);
-
-    if ($query->have_posts()) :
-        while ($query->have_posts()) : $query->the_post();
-            $author_id = get_the_author_meta('ID');
-            $avatar = get_avatar($author_id, 32); 
-            ?>
-            <div class="appostli-post-card">
-                <div class="appostli-card-meta">
-                    <?php if ($avatar) echo '<div class="appostli-retro-avatar">' . $avatar . '</div>'; ?>
-                    <div class="appostli-card-meta-text">
-                        <span class="appostli-author">BY: <?php the_author(); ?></span><br>
-                        <span class="appostli-date"><?php echo get_the_date('d M Y'); ?></span>
-                    </div>
-                </div>
-                <h3 class="appostli-card-title">
-                    <a href="<?php the_permalink(); ?>">> <?php the_title(); ?></a>
-                </h3>
-            </div>
-            <?php
-        endwhile;
-    endif;
-    wp_reset_postdata();
-    die();
